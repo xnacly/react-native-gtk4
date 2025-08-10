450040eeb86178ec19c5c34fd661a8bf62e75e2f
xnacly <47723417+xNaCly@users.noreply.github.com>
2025-08-10 16:20:20 +0200

    gtk4: init window with hello world

--- packages/react-native/ReactGTK4/main.cpp
+++ packages/react-native/ReactGTK4/main.cpp
@@ -1,7 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <gtk/gtk.h>
 
-int main() {
-  puts("Hello World from react native gtk4");
-  return EXIT_SUCCESS;
+static void on_activate(GApplication* app, gpointer user_data) {
+  GtkWindow* window =
+      GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(app)));
+  gtk_window_set_title(window, "Porting rn to GTK4");
+  gtk_window_set_default_size(window, 400, 200);
+
+  GtkWidget* label = gtk_label_new("Hello from react native");
+  gtk_window_set_child(window, label);
+
+  gtk_window_present(window);
+}
+
+int main(int argc, char** argv) {
+  GtkApplication* app =
+      gtk_application_new("com.example.gtk4", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+  return status;
 }
